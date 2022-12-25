3b7e6be36772184fba2fd42f81b8c078c558757f
kiskoza <kiskoza@users.noreply.github.com>
2022-12-25 16:04:36 +0000

    deploy: 7c91045a51aa01e08803bf28fbe5439c35018943

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "be6652c5eeca45bcb25a1f9989d094c8",
+  "main.dart.js": "fd28812d52a8575d721aadb9843c2daa",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "a416716ec65fcf839c13866dd83c084a",
-"index.html": "5690ac94ff5e1db1638690fb195a9796",
-"/": "5690ac94ff5e1db1638690fb195a9796",
+"index.html": "8730e6e01b0db061f6dc032c5754bde8",
+"/": "8730e6e01b0db061f6dc032c5754bde8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "62d03f6f2002fa95c8a7dea4c34d322f"
 };
